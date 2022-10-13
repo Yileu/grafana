1a89a2b3715b6d3801fa61b89bbee7ecb3b897d8
Yileu <trietpl1999@gmail.com>
2022-10-13 20:47:04 +0700

    sad

--- setup.sh
+++ setup.sh
@@ -1,4 +1,30 @@
+sudo yum install -y yum-utils
+sudo yum-config-manager \
+    --add-repo \
+    https://download.docker.com/linux/centos/docker-ce.repo
+sleep 5
+sudo yum install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
+systemctl enable docker
+systemctl start docker
+
+
+
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sleep 5
+sudo chmod +x /usr/local/bin/docker-compose
+/usr/local/bin/docker-compose --version
+sleep 5
+yum install -y git
+
+sleep 5
+
+git clone https://github.com/Yileu/grafana.git
+
+sleep 5
+
 ipadd=`echo $(hostname -I) | awk '{print $1}'`
 echo $ipadd
-sed -i "s|localhost|$ipadd|g" /prometheus/prometheus.yml
+sed -i "s|localhost|$ipadd|g" prometheus/prometheus.yml
+
 
